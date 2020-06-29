912853655cc9a8805eca90b6c7dc4c3c58ceeae9
Nauro <nauro@outlook.com>
2020-06-29 03:37:45 -0300

    Add return

--- serve.py
+++ serve.py
@@ -1,7 +1,8 @@
 from flask import (
     Flask,
     jsonify,
-    request
+    request,
+    send_file
 )
 
 from images import (
@@ -15,11 +16,17 @@ app = Flask(__name__)
 
 @app.route('/image', methods=['POST'])
 def image():
-    output_filename = str(uuid.uuid4()) + ".jpg"
+    uuid_return = str(uuid.uuid4())
+    output_filename = uuid_return + ".jpg"
 
     data = request.json
     save_image(image_with_text(data['name']), ("temp/" + output_filename) )
-    return jsonify({'filename' : output_filename})
+    return jsonify({'filename' : uuid_return})
+
+@app.route('/image', methods=['GET'])
+def get_image():
+    id = request.args.get('id').replace(".", "")
+    return send_file('temp/' + id + '.jpg', mimetype='image/jpg')
 
 if __name__ == '__main__':
     app.run(debug=True)
